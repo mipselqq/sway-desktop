da47178777b4ea827f32b6141fa72e69cd75a13b
mipselqq <mipselqq@gmail.com>
2025-09-13 14:18:18 +0300

    feat: i don't remember

--- install.sh
+++ install.sh
@@ -4,10 +4,7 @@ USER=lord
 HOME_DIR=/home/$USER
 SRC_DIR=$(pwd)
 
-pacman -S sway kitty wmenu copyq gammastep brightnessctl grim slurp wl-clipboard waybar libsecret breeze --noconfirm --needed
-
-mkdir -p "$HOME_DIR/.config"
-mkdir -p "$HOME_DIR/.local/share/applications"
+pacman -S sway kitty wmenu copyq qt6ct gammastep brightnessctl grim slurp xdg-desktop-portal xdg-desktop-portal-wlr wl-clipboard waybar libsecret breeze --noconfirm --needed
 
 rm -rf "$HOME_DIR/.config/sway"
 ln -s "$SRC_DIR/sway" "$HOME_DIR/.config/sway"
@@ -15,8 +12,8 @@ ln -s "$SRC_DIR/sway" "$HOME_DIR/.config/sway"
 rm -rf "$HOME_DIR/.config/waybar"
 ln -s "$SRC_DIR/waybar" "$HOME_DIR/.config/waybar"
 
-rm -f "$HOME_DIR/.local/share/applications/google-chrome.desktop"
-ln -s "$SRC_DIR/google-chrome.desktop" "$HOME_DIR/.local/share/applications/google-chrome.desktop"
+rm -f "/usr/share/applications/google-chrome.desktop"
+ln -s "$SRC_DIR/google-chrome.desktop" "/usr/share/applications/google-chrome.desktop"
 
 rm -f /usr/share/wayland-sessions/sway.desktop
 ln -s "$SRC_DIR/sway/sway.desktop" /usr/share/wayland-sessions/sway.desktop
