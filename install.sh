a8be632c3c3e1cfbf445d6216ddfdc18d471ffa3
Ilya Semyonov <mipselqq@gmail.com>
2025-10-09 22:18:40 +0300

    feat: make rofi and waybar improvements

--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ USER=lord
 HOME_DIR=/home/$USER
 SRC_DIR=$(pwd)
 
-pacman -S sway kitty wmenu copyq qt6ct gammastep brightnessctl grim slurp libadwaita xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr wl-clipboard waybar libsecret breeze --noconfirm --needed
+pacman -S sway kitty wmenu copyq qt6ct gammastep brightnessctl grim slurp libadwaita xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr wl-clipboard waybar libsecret breeze rofi htop fastfetch noto-fonts noto-fonts-cjk noto-fonts-extra ttf-liberation ttf-dejavu ttf-roboto snapper --noconfirm --needed
+
+if [ ! -f "/bin/yay" ]; then
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    rm . -rf
+fi
 
 rm -rf "$HOME_DIR/.config/sway"
 ln -s "$SRC_DIR/sway" "$HOME_DIR/.config/sway"
