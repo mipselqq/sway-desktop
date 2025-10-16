19b10a970abc06ef43c4a58c38d6924dac93a09c
Ilya <mipselqq@gmail.com>
2025-10-16 14:43:40 +0300

    fix: i don't quite remember

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ update_mirrorlist() {
 CAT_DE="sway waybar kitty rofi copyq qt6ct gammastep grim slurp xdg-desktop-portal
        xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr"
 
-CAT_APPS="telegram-desktop discord throne-bin apidog-bin
+CAT_APPS="qbittorrent firefox telegram-desktop discord throne-bin apidog-bin"
 	 
 CAT_THEMING="qt6ct breeze libadwaita"
 CAT_UTILS="wl-clipboard fzf fastfetch htop"
@@ -58,7 +58,7 @@ CAT_AUTH="libsecret"
 CAT_DEV="git docker docker-compose rustup nodejs npm neovim vim ripgrep visual-studio-code-bin"
 CAT_FONTS="noto-fonts noto-fonts-cjk noto-fonts-extra ttf-liberation ttf-dejavu ttf-roboto"
 CAT_DURABILITY="snapper"
-CAT_VIRT="virt-manager qemu-desktop libvirt edk2-vmf dnsmasq iptables-nft"
+CAT_VIRT="virt-manager qemu-desktop libvirt edk2-vmf dnsmasq"
 CAT_ARCHIEVES="tar 7zip unzip"
 CAT_MEDIA="pipwire pipewire-pulse aimp vlc"
 CAT_COMPAT="xorg-xwayland"
@@ -93,10 +93,12 @@ mkdir_ln_fsn "$SRC_DIR/rofi" "$HOME_DIR/.config/rofi"
 mkdir_ln_fsn "$SRC_DIR/shells/fish" "$HOME_DIR/.config/fish"
 mkdir_ln_fsn "$SRC_DIR/desktops/google-chrome.desktop" "$HOME_DIR/.local/share/applications/google-chrome.desktop"
 mkdir_ln_fsn "$SRC_DIR/desktops/sway/sway.desktop" "$HOME_DIR/.local/share/wayland-sessions/sway.desktop"
-mkdir_ln_fsn "$SRC_DIR/units/clashd.service" "/etc/systemd/system/clashd.service"
 mkdir_ln_fsn "$SRC_DIR/etc/fonts.conf" "/etc/fonts/fonts.conf"
 echo "INFO: enabling services"
-systemctl enable --now clashd
+
+echo "INFO: switching shell"
+chsh root -s /bin/fish
+chsh lord -s /bin/fish
 
 echo "INFO: done. Note you should install drivers ."
 
