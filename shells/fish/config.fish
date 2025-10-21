a2ab24f6632f8dd86fb51ebc3b369cf26028a9eb
Ilya <mipselqq@gmail.com>
2025-10-21 17:38:31 +0300

    feat: add ls shortcut in fish

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -1,3 +1,9 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+function set_bindings
+    bind -M insert alt-c __fish_list_current_token
+    bind -M default alt-c __fish_list_current_token
 end
+
+if status is-interactive
+    set_bindings
+end
+
